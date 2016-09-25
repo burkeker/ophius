3405ea8a7182f0640c101cfb87de6fbcd3aeca74
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-09-25 22:54:21 +0200

    [draft]

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+'use strict';
+
 var express = require('express');
-var path = require('path');
+//var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +15,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'nunjucks');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,6 +29,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+
+app.get('/', function(req, res){
+    res.type('text/plain');
+    res.send('Visualisation');
+});
+
+app.get('/participation', function(req, res){
+    res.type('text/plain');
+    res.send('Participation');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
